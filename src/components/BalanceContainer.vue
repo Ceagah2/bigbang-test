a72836f6deaea51e19653c16bea24504d0b17d72
Carlos Ceagah <carlosceagah@gmail.com>
2024-03-19 19:42:34 -0300

    feat: create balance container and balance item components

---
+++ src/components/BalanceContainer.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="balance-container">
+    <div class="container-header">
+      <div class="title-container">
+        <img class="icon" src="../assets/balance.png" alt="transactions icon" />
+        <h3 class="title">Análise geral</h3>
+        <h4 class="subtitle">últimos 7 dias</h4>
+      </div>
+      <span class="menu">Ver todas</span>
+    </div>
+    <div class="transactions-container">
+      <div class="balance">
+        <div class="balance-value-container">
+          <span class="balance-title">Ganhos</span>
+          <span class="balance-value">R$ {{ balance.incoming }}</span>
+        </div>
+        <div class="balance-value-container">
+          <span class="balance-title">Saidas</span>
+          <span class="balance-value">R$ {{ balance.outgoing }}</span>
+        </div>
+      </div>
+      <BalanceItem
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :name="transaction.name"
+        :value="transaction.value"
+        :color="transaction.color"
+        :icon="transaction.icon"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { balance, transactions } from '../constants/mock'
+import BalanceItem from './BalanceItem.vue'
+export default {
+  name: 'BalanceContainer',
+  components: {
+    BalanceItem
+  },
+  data() {
+    return {
+      balance,
+      transactions
+    }
+  },
+  mounted() {
+    console.log(transactions)
+  }
+}
+</script>
+
+<style scoped>
+.balance-container {
+  width: 90%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.container-header {
+  width: 80%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.icon {
+}
+.title-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  text-align: center;
+}
+.title {
+  font-family: 'Poppins';
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 30px;
+  text-align: left;
+  color: #202027;
+}
+.subtitle {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: left;
+  color: #a3a3a3;
+}
+.menu {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  color: #7986fe;
+}
+.menu:hover {
+  cursor: pointer;
+}
+.transactions-container {
+  width: 80%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.balance {
+  width: 100%;
+  height: 85px;
+  border-bottom: 1px solid #b3b3b3;
+  border-top: 1px solid #b3b3b3;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.balance-value-container {
+  width: 20%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  text-align: center;
+}
+.balance-value {
+  font-family: 'Poppins';
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 30px;
+  text-align: left;
+  color: #000000;
+}
+.balance-title {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: left;
+  color: #000000;
+}
+</style>
---
+++ src/components/BalanceItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="balance-item">
+    <div class="text-container">
+      <div class="title-container">
+        <img :src="icon" :alt="title" style="color: black; margin-bottom: 10px" />
+        <span class="title">{{ name }}</span>
+      </div>
+      <span class="value">{{ value }} %</span>
+    </div>
+    <div class="range-bar">
+      <div class="range" :style="{ backgroundColor: color, width: value + '%' }"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BalanceItem',
+  props: {
+    icon: String,
+    name: String,
+    value: Number,
+    color: String
+  }
+}
+</script>
+
+<style scoped>
+.balance-item {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.text-container {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 20px;
+  text-align: center;
+}
+.title-container {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+.title,
+.value {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: left;
+  color: #000000;
+  margin-top: -10px;
+}
+
+.range-bar {
+  width: 100%;
+  height: 10px;
+  border-radius: 5px;
+  background-color: #d9d9d9;
+}
+.range {
+  height: 100%;
+  border-radius: 5px;
+}
+</style>
