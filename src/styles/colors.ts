6ed22b8a7b937624ed4ebb61c2fd425e59089b55
Carlos Ceagah <carlosceagah@gmail.com>
2024-03-19 12:02:28 -0300

    feat: add initial style and theme config

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  /// Primary colors
+  background: '#202027',
+  primary: '#7986FE',
+  secondary: '#FF6969',
+  details: '#A3A3A3',
+  black: '#000000',
+  white: '#FFFFFF',
+
+  /// Secondary colors
+  yellow: '#FDFF9D',
+  red: '#FF9D9D',
+  green: '#C8FF9D',
+  purple: '#AB9DFF',
+  pink: '#FF9DEF'
+}
---
+++ src/styles/sizes.ts
@@ -0,0 +1,6 @@
+export const sizes = {
+  focused: '36px',
+  heading: '20px',
+  body: '16px',
+  small: '14px'
+}
