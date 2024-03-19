fe6bcf34066434d7c8181f9dbbfc5aa6b4278a76
Carlos Ceagah <carlosceagah@gmail.com>
2024-03-19 17:12:59 -0300

    feat: create card values container

---
+++ src/components/Card.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="card-container">
+    <span class="card-title">{{ title }}</span>
+    <span class="card-value">R$ {{ value }}</span>
+    <div class="buttons-container">
+      <button class="details-button">Ver mais detalhes</button>
+      <button class="menu-button">
+        <i class="fa-solid fa-bars"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ValueCard',
+  props: {
+    title: String,
+    value: String
+  }
+}
+</script>
+
+<style scoped>
+.card-container {
+  width: 350px;
+  height: 220px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 20px;
+  background: white;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+}
+.card-title {
+  font-family: 'Poppins', sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  text-align: left;
+  color: #000000;
+}
+.card-value {
+  font-family: 'Poppins', sans-serif;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 36px;
+  text-align: left;
+  color: #000000;
+}
+.buttons-container {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.details-button {
+  width: 70%;
+  font-family: 'Poppins', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 21px;
+  text-align: left;
+  color: #686868;
+  border: 1px solid #747474;
+  padding: 10px;
+  text-align: center;
+}
+.menu-button {
+  width: 50px;
+  height: 35px;
+  border: 1px solid #747474;
+}
+</style>
---
+++ src/components/CashContainer.vue
@@ -0,0 +1,196 @@
+<template>
+  <div class="cash-container">
+    <div class="container-header">
+      <div class="header-text-container">
+        <div class="value-container">
+          <h3>Saldo total</h3>
+          <div class="values-container">
+            <span class="value">R$ {{ total.toFixed(2) }}</span>
+            <span style="color: yellow; margin: 0 10px">
+              <i class="fa-solid fa-bolt fa-2xl"></i>
+            </span>
+            <span style="color: white">rendendo 124%</span>
+          </div>
+        </div>
+        <div class="buttons-container">
+          <button class="send-button">
+            <i class="fa-brands fa-airbnb fa-l" style="transform: rotate(45deg)"></i>
+            Enviar dinheiro
+          </button>
+          <button class="add-button">
+            <i class="fa-solid fa-plus fa-l"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="money-container">
+      <div class="cards-title">
+        <img src="../assets/bubble.png" alt="bubbles" />
+        <h3 class="title">Painel de estatísticas</h3>
+        <span class="subtitle">últimos 7 dias</span>
+      </div>
+      <div class="cards-container">
+        <ValueCard title="Conta Pj" :value="pj" />
+        <ValueCard title="Recebimentos" :value="incoming" />
+        <ValueCard title="Lucro" :value="profit" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { values } from '../constants/mock'
+import ValueCard from './Card.vue'
+
+export default {
+  name: 'CashContainer',
+  components: {
+    ValueCard
+  },
+  data() {
+    return {
+      total: values.total,
+      pj: values.pj,
+      incoming: values.incoming,
+      profit: values.profit
+    }
+  }
+}
+</script>
+
+<style scope>
+.cash-container {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 0 auto;
+  padding-top: 20px;
+  background: linear-gradient(#202027 60%, white 60%);
+}
+.container-header {
+  width: 80%;
+  height: 20%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.header-text-container {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-left: 20px;
+}
+
+.value-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  text-align: left;
+}
+.value-container > h3 {
+  font-family: 'Poppins', sans-serif;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 30px;
+  text-align: center;
+  color: white;
+}
+.values-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.value {
+  font-family: 'Poppins', sans-serif;
+  font-size: 36px;
+  font-weight: 500;
+  line-height: 54px;
+  text-align: left;
+  color: white;
+}
+.buttons-container {
+  width: 40%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+}
+.send-button {
+  width: 254px;
+  height: 37px;
+  background: #7986fe;
+  font-family: 'Poppins', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: center;
+  color: white;
+  border: none;
+}
+.add-button {
+  width: 56px;
+  height: 37px;
+  background: #7986fe;
+  font-family: 'Poppins', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: center;
+  color: white;
+  border: none;
+}
+button:hover {
+  cursor: pointer;
+  opacity: 0.8;
+}
+.cards-title {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 20px;
+}
+.title {
+  font-family: 'Poppins', sans-serif;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 30px;
+  text-align: center;
+  color: white;
+  margin-left: 10px;
+}
+.subtitle {
+  font-family: 'Poppins', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  color: #a3a3a3;
+  margin: 10px 20px;
+}
+.money-container {
+  width: 80%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.cards-container {
+  width: 80%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+}
+</style>
