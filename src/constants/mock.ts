2d3d7b0d47b1be6fee7647307e4a06864c16c363
Carlos Ceagah <carlosceagah@gmail.com>
2024-03-19 12:02:41 -0300

    feat: create mock file

---
+++ src/constants/mock.ts
@@ -0,0 +1,57 @@
+export const values = {
+  total: 304.78,
+  pj: 398.82,
+  incoming: 765.88,
+  profit: 158.87
+}
+
+export const locations = [
+  {
+    id: 1,
+    name: 'Mercadinho da cidade',
+    value: 10.98
+  },
+  {
+    id: 2,
+    name: 'Tenda Nostra',
+    value: 26.38
+  },
+  {
+    id: 3,
+    name: 'Farmácia um',
+    value: 43.17
+  },
+  {
+    id: 4,
+    name: 'Academia Body',
+    value: 62.12
+  },
+  {
+    id: 5,
+    name: 'ShopOnline',
+    value: 135.24
+  }
+]
+
+export const balance = {
+  incoming: 2789.21,
+  outgoing: 1278.47
+}
+
+export const transactions = [
+  {
+    id: 1,
+    name: 'Compras',
+    value: 62
+  },
+  {
+    id: 2,
+    name: 'Boletos',
+    value: 14
+  },
+  {
+    id: 3,
+    name: 'Investimentos',
+    value: 5
+  }
+]
