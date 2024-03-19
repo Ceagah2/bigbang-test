3454541477b0530bd7fa5d80072b148f6baaf246
Carlos Ceagah <carlosceagah@gmail.com>
2024-03-19 17:56:42 -0300

    feat: finish transaction section with transaction item component

---
+++ src/components/TransactionItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="transaction-item">
+    <div class="icon-container" :style="{ backgroundColor: color }">
+      <img src="../assets/shop.png" alt="shop icon" />
+    </div>
+    <div class="transaction-details">
+      <span class="transaction-name">{{ name }}</span>
+      <span class="transaction-value">R${{ value }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionItem',
+  props: {
+    color: String,
+    name: String,
+    value: String
+  }
+}
+</script>
+
+<style scoped>
+.transaction-item {
+  width: 80%;
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #b3b3b3;
+}
+.icon-container {
+  width: 40px;
+  height: 40px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.icon {
+}
+.transaction-details {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+}
+.transaction-name {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: left;
+  color: #202027;
+}
+.transaction-value {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: right;
+  color: #000000;
+}
+</style>
---
+++ src/components/Transactions.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="transactions-container">
+    <div class="container-header">
+      <div class="title-container">
+        <img class="icon" src="../assets/transactions.png" alt="transactions icon" />
+        <h3 class="title">Transações recentes</h3>
+        <h4 class="subtitle">últimos 7 dias</h4>
+      </div>
+      <span class="menu">Ver todas</span>
+    </div>
+    <div class="transactions-container">
+      <TransactionItem
+        v-for="location in locations"
+        :key="location.id"
+        :color="location.color"
+        :name="location.name"
+        :value="location.value"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TransactionItem from './TransactionItem.vue'
+import { locations } from '../constants/mock'
+
+export default {
+  name: 'TransactionsContainer',
+  components: {
+    TransactionItem
+  },
+  data() {
+    return {
+      locations
+    }
+  }
+}
+</script>
+
+<style scoped>
+.transactions-container {
+  width: 40%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.container-header {
+  width: 80%;
+  height: 20%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.icon {
+}
+.title-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  text-align: center;
+}
+.title {
+  font-family: 'Poppins';
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 30px;
+  text-align: left;
+  color: #202027;
+}
+.subtitle {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  text-align: left;
+  color: #a3a3a3;
+}
+.menu {
+  font-family: 'Poppins';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  color: #7986fe;
+}
+.menu:hover {
+  cursor: pointer;
+}
+
+.transactions-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
